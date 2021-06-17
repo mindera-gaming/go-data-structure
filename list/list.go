58eca85dc89f5bff5c7991b509004a9fca23193e
Gonçalo Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2021-06-17 14:20:51 +0100

    Code optimization

--- list/list.go
+++ list/list.go
@@ -2,20 +2,28 @@ package list
 
 import "container/list"
 
-// PreviousCircularElement returns the previous list element in a circular way.
-func PreviousCircularElement(list list.List, current list.Element) list.Element {
+// PreviousCircularElement returns the previous list element in a circular way, or nil if there is no such element.
+func PreviousCircularElement(list *list.List, current *list.Element) *list.Element {
+	if list == nil || current == nil {
+		return nil
+	}
+
 	element := current.Prev()
 	if element == nil {
 		element = list.Back()
 	}
-	return *element
+	return element
 }
 
-// NextCircularElement returns the next list element in a circular way.
-func NextCircularElement(list list.List, current list.Element) list.Element {
+// NextCircularElement returns the next list element in a circular way, or nil if there is no such element.
+func NextCircularElement(list *list.List, current *list.Element) *list.Element {
+	if list == nil || current == nil {
+		return nil
+	}
+
 	element := current.Next()
 	if element == nil {
 		element = list.Front()
 	}
-	return *element
+	return element
 }
