3c48588e5abe553ff599977fc16240602865b14d
Gonçalo Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2021-06-18 12:31:35 +0100

    Small fix on the package list

--- list/list.go
+++ list/list.go
@@ -3,19 +3,19 @@ package list
 import "container/list"
 
 // PreviousCircularElement returns the previous list element in a circular way.
-func PreviousCircularElement(list list.List, current list.Element) list.Element {
+func PreviousCircularElement(list *list.List, current *list.Element) *list.Element {
 	element := current.Prev()
 	if element == nil {
 		element = list.Back()
 	}
-	return *element
+	return element
 }
 
 // NextCircularElement returns the next list element in a circular way.
-func NextCircularElement(list list.List, current list.Element) list.Element {
+func NextCircularElement(list *list.List, current *list.Element) *list.Element {
 	element := current.Next()
 	if element == nil {
 		element = list.Front()
 	}
-	return *element
+	return element
 }
